2965a87818b2459e5a741be210eee386f6edf006
Freddy <1391977+f-ewald@users.noreply.github.com>
2025-01-26 13:22:16 -0800

    Use Singleton logger (#6)

--- worklog/worklog.rb
+++ worklog/worklog.rb
@@ -6,13 +6,9 @@ require_relative 'daily_log'
 require_relative 'log_entry'
 require_relative 'storage'
 
-require 'logger'
 require 'optparse'
 require 'rainbow'
 require 'yaml'
-
-# Initialize logger
-$logger = Logger.new(STDOUT, level: Logger::Severity::INFO)
 
 module Worklog
 end
