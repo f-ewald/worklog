b92e815b7d4958c4a6f1688dcbf62d0a663163d6
Friedrich Ewald <fewald@linkedin.com>
2025-03-03 16:34:32 -0800

    Add coverage data

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 require 'simplecov'
+require 'simplecov-cobertura'
 
 # Needed to generate coverage reports.
 # Otherwise the report will be generated too early, before the tests are run.
 SimpleCov.external_at_exit = true
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 
 SimpleCov.start do
   add_filter '/test/'
