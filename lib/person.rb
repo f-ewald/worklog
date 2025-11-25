ca24cd922e412f7ee9932e569dbf814c2a3e3fc3
Friedrich Ewald <freddiemailster@gmail.com>
2025-11-25 09:24:01 -0800

    Refactor Person into Worklog module
    Also simplify Person instantiation

--- lib/person.rb
+++ lib/person.rb
@@ -1,57 +1,62 @@
 # frozen_string_literal: true
 
-# Represents a person at work.
-#
-# !attribute [r] handle
-#  @return [String] The person's handle (username)
-# !attribute [r] name
-#  @return [String] The person's full name
-# !attribute [r] email
-#  @return [String, nil] The person's email address, can be nil
-# !attribute [r] team
-#  @return [String, nil] The team the person belongs to, can be nil
-# !attribute [r] notes
-#  @return [Array<String>] An array of notes about the person
-class Person
-  attr_reader :handle, :name, :email, :team, :notes
+module Worklog
+  # Represents a person at work.
+  #
+  # !attribute [r] handle
+  #  @return [String] The person's handle (username)
+  # !attribute [r] name
+  #  @return [String] The person's full name
+  # !attribute [r] email
+  #  @return [String, nil] The person's email address, can be nil
+  # !attribute [r] team
+  #  @return [String, nil] The team the person belongs to, can be nil
+  # !attribute [r] notes
+  #  @return [Array<String>] An array of notes about the person
+  class Person
+    attr_reader :handle, :github_username, :name, :email, :team, :notes
 
-  def initialize(handle:, name:, email:, team:, notes: [])
-    @handle = handle
-    @name = name
-    @email = email
-    @team = team
-    @notes = notes
-  end
+    def initialize(handle:, name:, **params)
+      # params to symbol keys
+      params = params.transform_keys(&:to_sym)
 
-  # Creates a new Person instance from a hash of attributes.
-  # @param hash [Hash] A hash containing person attributes
-  # @option hash [String] :handle The person's handle (username)
-  # @option hash [String] :name The person's full name
-  # @option hash [String, nil] :email The person's email address, can be nil
-  # @option hash [String, nil] :team The team the person belongs to, can be nil
-  # @option hash [Array<String>] :notes An array of notes about the person
-  # @return [Person] A new Person instance
-  def self.from_hash(hash)
-    raise ArgumentError, 'Person handle is required' unless hash[:handle] || hash['handle']
-    raise ArgumentError, 'Person name is required' unless hash[:name] || hash['name']
+      @handle = handle
+      @name = name
+      @github_username = params[:github_username]
+      @email = params[:email]
+      @team = params[:team]
+      @notes = params[:notes] || []
+    end
 
-    handle = hash[:handle] || hash['handle']
-    name = hash[:name] || hash['name']
-    email = hash[:email] || hash['email']
-    team = hash[:team] || hash['team']
-    notes = hash[:notes] || hash['notes'] || []
-    Person.new(handle: handle, name: name, email: email, team: team, notes: notes)
-  end
+    # Creates a new Person instance from a hash of attributes.
+    # @param hash [Hash] A hash containing person attributes
+    # @option hash [String] :handle The person's handle (username)
+    # @option hash [String] :name The person's full name
+    # @option hash [String, nil] :email The person's email address, can be nil
+    # @option hash [String, nil] :team The team the person belongs to, can be nil
+    # @option hash [Array<String>] :notes An array of notes about the person
+    # @return [Person] A new Person instance
+    def self.from_hash(hash)
+      hash = hash.transform_keys(&:to_sym)
 
-  def to_s
-    return "#{name} (~#{handle})" if @email.nil?
+      raise ArgumentError, 'Person handle is required' unless hash[:handle]
+      raise ArgumentError, 'Person name is required' unless hash[:name]
 
-    "#{name} (~#{handle}) <#{email}>"
-  end
+      handle = hash[:handle]
+      name = hash[:name]
+      Person.new(handle: handle, name: name, **hash)
+    end
 
-  def ==(other)
-    return false unless other.is_a?(Person)
+    def to_s
+      return "#{name} (~#{handle})" if @email.nil?
 
-    handle == other.handle && name == other.name && email == other.email && team == other.team && notes == other.notes
+      "#{name} (~#{handle}) <#{email}>"
+    end
+
+    def ==(other)
+      return false unless other.is_a?(Person)
+
+      handle == other.handle && name == other.name && email == other.email && team == other.team && notes == other.notes
+    end
   end
 end
