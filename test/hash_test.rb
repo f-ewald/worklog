fe18c174e58fe004525523ff845a01125bc3fdfc
Freddy <1391977+f-ewald@users.noreply.github.com>
2025-12-17 10:24:37 -0800

    Refactor to_hash method (#93)

--- test/hash_test.rb
+++ test/hash_test.rb
@@ -9,6 +9,23 @@ class HashTest < Minitest::Test
     @hash = Hash.new
   end
 
+  def test_to_hash
+    test_class = Class.new do
+      include Hashify
+
+      attr_accessor :a, :b
+
+      def initialize(a, b)
+        @a = a
+        @b = b
+      end
+    end
+
+    obj = test_class.new(1, 'test')
+    expected_hash = { a: 1, b: 'test' }
+    assert_equal(expected_hash, obj.to_hash)
+  end
+
   def test_stringify_keys
     hash = { 'a' => 1, 'b' => 2 }
     assert_equal({ 'a' => 1, 'b' => 2 }, hash.stringify_keys)
